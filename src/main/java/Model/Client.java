afcca84e7bee48cf5dc42ad7e704fe6f2208c630
unknown <razvan0721@gmail.com>
2026-05-14 09:45:07 +0300

    client done

--- src/main/java/Model/Client.java
+++ src/main/java/Model/Client.java
@@ -1,4 +1,34 @@
 package Model;
 
 public class Client {
+    private int id;
+    private String name;
+    public Client(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
